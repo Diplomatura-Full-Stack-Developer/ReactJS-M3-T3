efcda0aa1f74e70b1c0901a7c4dfc6a0ec45996b
Rubén Seco <ruben.seco.rs@gmail.com>
2026-08-05 16:07:35 -0300

    Add product form and validation to AddProductPage and UpdateProductPage
    - Implement form handling using react-hook-form for adding and updating products.
    - Add validation for price and stock fields to ensure correct input.
    - Enhance user experience with error messages for invalid inputs.
    - Update form structure and styling for consistency across product management pages.

--- src/features/products/AddProductPage.jsx
+++ src/features/products/AddProductPage.jsx
@@ -1,6 +1,58 @@
+import { addProduct } from './services/products.service';
+import { useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 
 export const AddProductPage = () => {
+  const navigate = useNavigate();
+
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const onSubmit = async (data) => {
+    event.preventDefault();
+    console.log(data);
+    // await addProduct(data);
+    navigate('dashboard');
+  };
+
   return (
-    <div>AddProductPage</div>
-  )
-}
+    <div>
+      <div className='bg-primary-200 flex flex-col items-center justify-center mx-4 p-4 md:mx-8 rounded-lg shadow-lg'>
+        <form
+          className='flex flex-col items-center justify-center gap-2'
+          onSubmit={handleSubmit(onSubmit)}>
+          <label
+            className='text-secondary-600 text-shadow-lg text-lg font-bold text-center'
+            htmlFor='name'>
+            Precio
+          </label>
+          <input
+            className='border-2 border-primary-700 rounded-md p-2'
+            type='number'
+            {...register('price', { required: true, min: 1 })}
+          />
+          {errors.price && <p className='text-accent-700 text-sm'>El precio es requerido</p>}
+          {errors.price && <p className='text-accent-700 text-sm'>El precio debe ser mayor a 0</p>}
+          {errors.price && <p className='text-accent-700 text-sm'>El precio debe ser un número</p>}
+          <label
+            className='text-secondary-600 text-shadow-lg text-lg font-bold text-center'
+            htmlFor='brand'>
+            Stock
+          </label>
+          <input
+            className='border-2 border-primary-700 rounded-md p-2'
+            type='number'
+            {...register('stock', { required: true, min: 0 })}
+          />
+          {errors.stock && <p className='text-accent-700 text-sm'>El stock es requerido</p>}
+          {errors.stock && <p className='text-accent-700 text-sm'>El stock debe ser mayor o igual a 0</p>}
+          {errors.stock && <p className='text-accent-700 text-sm'>El stock debe ser un número</p>}
+          <button
+            className='bg-primary-600 text-white/80 text-shadow-lg text-xl font-bold text-center p-2 rounded-lg hover:bg-primary-700 transition-all duration-300 mt-2 cursor-pointer'
+            type='submit'>
+            Agregar producto
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
--- src/features/products/UpdateProductPage.jsx
+++ src/features/products/UpdateProductPage.jsx
@@ -16,7 +16,11 @@ export const UpdateProductPage = () => {
     fetchProduct();
   }, [id, product]);
 
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   const onSubmit = async (data) => {
     event.preventDefault();
@@ -32,12 +36,42 @@ export const UpdateProductPage = () => {
     <div>
       <ShowProductPage showButtons={false} />
       <div className='bg-primary-200 flex flex-col items-center justify-center mx-4 p-4 md:mx-8 rounded-lg shadow-lg'>
-        <form className='flex flex-col items-center justify-center gap-2' onSubmit={handleSubmit(onSubmit)}>
-          <label className='text-secondary-600 text-shadow-lg text-lg font-bold text-center' htmlFor='name'>Precio</label>
-          <input className='border-2 border-primary-700 rounded-md p-2' type='number' {...register('price')} defaultValue={product.price} />
-          <label className='text-secondary-600 text-shadow-lg text-lg font-bold text-center' htmlFor='brand'>Stock</label>
-          <input className='border-2 border-primary-700 rounded-md p-2' type='number' {...register('stock')} defaultValue={product.stock} />
-          <button className='bg-primary-600 text-white/80 text-shadow-lg text-xl font-bold text-center p-2 rounded-lg hover:bg-primary-700 transition-all duration-300 mt-2' type='submit'>Modificar datos</button>
+        <form
+          className='flex flex-col items-center justify-center gap-2'
+          onSubmit={handleSubmit(onSubmit)}>
+          <label
+            className='text-secondary-600 text-shadow-lg text-lg font-bold text-center'
+            htmlFor='name'>
+            Precio
+          </label>
+          <input
+            className='border-2 border-primary-700 rounded-md p-2'
+            type='number'
+            defaultValue={product.price}
+            {...register('price', { required: true, min: 1 })}
+          />
+          {errors.price && <span className='text-accent-700 text-sm'>El precio es requerido</span>}
+          {errors.price && <span className='text-accent-700 text-sm'>El precio debe ser mayor a 0</span>}
+          {errors.price && <span className='text-accent-700 text-sm'>El precio debe ser un número</span>}
+          <label
+            className='text-secondary-600 text-shadow-lg text-lg font-bold text-center'
+            htmlFor='brand'>
+            Stock
+          </label>
+          <input
+            className='border-2 border-primary-700 rounded-md p-2'
+            type='number'
+            defaultValue={product.stock}
+            {...register('stock', { required: true, min: 0 })}
+          />
+          {errors.stock && <span className='text-accent-700 text-sm'>El stock es requerido</span>}
+          {errors.stock && <span className='text-accent-700 text-sm'>El stock debe ser mayor o igual a 0</span>}
+          {errors.stock && <span className='text-accent-700 text-sm'>El stock debe ser un número</span>}
+          <button
+            className='bg-primary-600 text-white/80 text-shadow-lg text-xl font-bold text-center p-2 rounded-lg hover:bg-primary-700 transition-all duration-300 mt-2 cursor-pointer'
+            type='submit'>
+            Modificar datos
+          </button>
         </form>
       </div>
     </div>
