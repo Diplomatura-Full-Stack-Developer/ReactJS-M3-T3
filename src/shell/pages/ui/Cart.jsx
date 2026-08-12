b1b8ae6184037bdb89865918d0e66e62ea2121ef
Rubén Seco <ruben.seco.rs@gmail.com>
2026-08-12 11:40:31 -0300

    Refactor Cart component layout and styling for improved responsiveness
    - Update Cart component to enhance layout with full-width and height adjustments.
    - Modify text sizes for better readability on different screen sizes.
    - Add padding for improved spacing and user experience.

--- src/shell/pages/ui/Cart.jsx
+++ src/shell/pages/ui/Cart.jsx
@@ -1,10 +1,10 @@
 import { Link } from 'react-router-dom';
 export const Cart = () => {
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen'>
-      <h1 className='text-4xl font-bold'>Pagina protegida</h1>
-      <p className='text-2xl'>Llegaste aquí porque iniciaste sesión</p>
-      <p className='text-2xl'>Pagina del carrito en construcción</p>
+    <div className='flex flex-col w-full items-center justify-center h-full p-4 md:p-8'>
+      <h1 className='text-2xl md:text-4xl font-bold'>Pagina protegida</h1>
+      <p className='md:text-2xl'>Llegaste aquí porque iniciaste sesión.</p>
+      <p className='md:text-2xl'>Pagina del carrito en construcción.</p>
       <Link to='/' type='button' className='font-bold text-blue-600 hover:text-blue-700 m-4 p-2 rounded-md border border-blue-600 hover:border-blue-700'>Volver a la página principal</Link>
     </div>
   )
